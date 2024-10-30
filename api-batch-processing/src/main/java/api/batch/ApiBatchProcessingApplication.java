0f08188646bc9dab9347745060396fd903d61d55
ElielRegiani <elielregiani@gmail.com>
2024-10-30 09:24:52 -0300

    First commit

---
+++ api-batch-processing/src/main/java/api/batch/ApiBatchProcessingApplication.java
@@ -0,0 +1,13 @@
+package api.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBatchProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBatchProcessingApplication.class, args);
+	}
+
+}
---
+++ api-batch-processing/src/test/java/api/batch/ApiBatchProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package api.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBatchProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
